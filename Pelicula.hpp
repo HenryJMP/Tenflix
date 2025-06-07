e8ddb4c441c46c24a470e60f3cc2a049dece7bdc
Enrique Jimenez-Moran Y Perez <a01635419@tec.mx>
2025-06-07 17:31:01 -0600

    Update

---
+++ Pelicula.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "Videos.hpp"
+
+class Pelicula : public Videos {
+    public:
+        // Constructor
+        Pelicula();
+        // Destructor
+        virtual ~Pelicula(){};
+        // Metodos de acceso
+
+
+    private:
+        
+};
--- Videos.hpp
+++ Videos.hpp
@@ -2,28 +2,25 @@
 #include <iostream>
 #include <vector>
 
-class Videos{
-    public:
-        //Constructor 
-        Videos(std::string, std::string, std::string, float);
-        //Destructor
-        virtual ~Videos(){};
-        //Metodos de acceso
-        std::string getID() const;
-        std::string getNombre() const;
-        std::string getGenero() const;
-        float getDuracion() const;
-        float getCalificacion() const;
-        //Extras
-        virtual float setCalificacion() const = 0;
+class Videos {
+public:
+    // Constructor 
+    Videos(std::string, std::string, std::string, float);
+    // Virtual destructor
+    virtual ~Videos() = default;
+    // Métodos de acceso
+    std::string getID() const;
+    std::string getNombre() const;
+    std::string getGenero() const;
+    float getDuracion() const;
+    float getCalificacion() const;
+    // Método para modificar la calificación
+    virtual void setCalificacion(float nuevaCalificacion) = 0;
 
-    protected:
-    
-    private:
-        std::string id;
-        std::string nombre;
-        std::string genero;
-        float duracion;
-        float calificacion;
-
+protected:
+    std::string id;
+    std::string nombre;
+    std::string genero;
+    float duracion;
+    float calificacion;
 };
